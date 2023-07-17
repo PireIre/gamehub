afac780a2c83a841402d6bc14afa4b05c276ae1b
Irenej <irenej@bambuser.com>
2023-07-17 11:39:32 +0200

    Add GameGrid Component

---
+++ src/components/GameGrid.tsx
@@ -0,0 +1,19 @@
+import GameCard from "./GameCard";
+import { SimpleGrid } from "@chakra-ui/react";
+import useGames from "../hooks/useGames";
+
+const GameGrid = () => {
+  const { games, error } = useGames();
+
+  return (
+    <>
+      <SimpleGrid minChildWidth="300px" spacing={10}>
+        {games.map((game) => (
+          <GameCard key={game.id} game={game} />
+        ))}
+      </SimpleGrid>
+    </>
+  );
+};
+
+export default GameGrid;
