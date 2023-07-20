d3f33779c92a833813407bdceb7a156fae844da9
Irenej <irenej@bambuser.com>
2023-07-20 10:54:26 +0200

    Fit images to cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
 import PlatformIcons from "./PlatformIcons";
 import { Card, Heading, CardBody, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
+import { getCroppedImageUrl } from "./../services/image-url";
 
 interface Props {
   game: Game;
@@ -11,9 +12,18 @@ const GameCard = ({ game }: Props) => {
     <>
       <Card borderRadius="30px">
         <CardBody>
-          <Image src={game.background_image} borderRadius="lg" />
+          <Image
+            objectFit="cover"
+            height="200px"
+            width="100%"
+            paddingBottom="15px"
+            src={getCroppedImageUrl(game.background_image)}
+            borderRadius="lg"
+          />
           <Heading fontSize="2xl">{game.name}</Heading>
-          <PlatformIcons platforms={game.parent_platforms?.map((p) => p.platform)} />
+          <PlatformIcons
+            platforms={game.parent_platforms?.map((p) => p.platform)}
+          />
         </CardBody>
       </Card>
     </>
